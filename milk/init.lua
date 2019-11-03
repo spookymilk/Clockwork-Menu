a0f920ea835a5722f5d9ebbefa95c6eb7e2b1fb8
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-03 16:49:42 -0500

    Small changes

--- milk/init.lua
+++ milk/init.lua
@@ -1,7 +1,5 @@
--- this code runs when all mods' filesystems are registered
-
-function OnPlayerSpawned( player_entity )
+function OnPlayerSpawned(player_entity)
 EntityLoad("data/milk.xml");
 end;
---ModMagicNumbersFileAdd( "mods/example/files/magic_numbers.xml" ) -- will override some magic numbers using the specified file
-print("Example mod init done")
+
+print("Milk Menu Initialized.");
