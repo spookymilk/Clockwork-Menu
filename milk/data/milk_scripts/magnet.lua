25927ada76c1447d368bcf51d1f1dca759ee6e74
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-06 06:35:50 -0500

    Script improvements

--- milk/data/milk_scripts/magnet.lua
+++ milk/data/milk_scripts/magnet.lua
@@ -5,57 +5,32 @@ dofile( "data/scripts/lib/utilities.lua" )
 -- more of a curse?
 
 local entity_id = GetUpdatedEntityID()
-local x, y = EntityGetTransform( entity_id )
 
-local items = EntityGetWithTag( "item_physics" )
-local more_items=EntityGetWithTag("mortal"); -- this may be a curse..
+function attract(entity,attractionTag,theDistance,strengthX,strengthY)
+local ex,ey=EntityGetTransform(entity);
+local allWithTag=EntityGetWithTag(attractionTag);
+if allWithTag~=nil then
+for _,v in pairs(allWithTag) do
+local ix,iy=EntityGetTransform(v);
+local distance=math.abs(ex-ix)+math.abs(ey-iy);
+local maxDistance=theDistance;
+if distance<maxDistance*1.25 then
+distance=math.sqrt((ex-ix)^2+(ey-iy)^2);
+direction=0-math.atan2((ey-iy),(ex-ix));
+if distance<maxDistance then
+local getPhysicsComp=EntityGetComponent(v,"PhysicsBodyComponent");
+if getPhysicsComp~=nil then
+local xVel=math.cos(direction)*strengthX;
+local yVel=0-math.sin(direction)*strengthY;
+PhysicsApplyForce(v,xVel,yVel);
+end;
+end;
+end;
+end;
+end;
+end;
 
-if ( items ~= nil) then
-	for i,item_id in ipairs(items) do	
-		local px, py = EntityGetTransform( item_id )
-		
-		local distance = math.abs( x - px ) + math.abs( y - py )
-		local distance_full=250; -- aa huh.
-		
-		if ( distance < distance_full * 1.25 ) then
-			distance = math.sqrt( ( x - px ) ^ 2 + ( y - py ) ^ 2 )
-			direction = 0 - math.atan2( ( y - py ), ( x - px ) )
-	
-			if ( distance < distance_full ) then
-				local physicscomponents = EntityGetComponent( item_id, "PhysicsBodyComponent" )
-				
-				if ( physicscomponents ~= nil ) then
-					local vel_x = math.cos( direction ) * 45; -- aa huh.
-					local vel_y = 0 - math.sin( direction ) * 45; -- aa huh.
-					
-					PhysicsApplyForce( item_id, vel_x, vel_y )
-				end
-			end
-		end
-	end
-end
-
-if ( more_items ~= nil) then
-	for i,more_items_id in ipairs(more_items) do	
-		local px, py = EntityGetTransform( more_items_id )
-		
-		local distance = math.abs( x - px ) + math.abs( y - py )
-		local distance_full=250; -- aa huh.
-		
-		if ( distance < distance_full * 1.25 ) then
-			distance = math.sqrt( ( x - px ) ^ 2 + ( y - py ) ^ 2 )
-			direction = 0 - math.atan2( ( y - py ), ( x - px ) )
-	
-			if ( distance < distance_full ) then
-				local physicscomponents = EntityGetComponent( more_items_id, "PhysicsBodyComponent" )
-				
-				if ( physicscomponents ~= nil ) then
-					local vel_x = math.cos( direction ) * 45; -- aa huh.
-					local vel_y = 0 - math.sin( direction ) * 45; -- aa huh.
-					
-					PhysicsApplyForce( more_items_id, vel_x, vel_y )
-				end
-			end
-		end
-	end
-end
+attract(entity_id,"item_physics",250,45,45);
+attract(entity_id,"mortal",250,45,45);
+--attract(entity_id,"helpless_animal",250,45,45);
+attract(entity_id,"teleportable_NOT",250,45,45);
