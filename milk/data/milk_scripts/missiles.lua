73ad4f28f00531977bd613708529226f211019ac
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-07 22:10:22 -0500

    Offset fix

--- milk/data/milk_scripts/missiles.lua
+++ milk/data/milk_scripts/missiles.lua
@@ -3,4 +3,4 @@ dofile("data/milk_scripts/functions.lua");
 
 local entity_id=GetUpdatedEntityID();
 
-attackOffset(entity_id,"destruction_target",65,0,-10,10,"data/milk_edited/curse_rocket.xml"); -- this is a curse, and dangerous.
+attackOffset(entity_id,"destruction_target",65,0,-20,10,"data/milk_edited/curse_rocket.xml"); -- this is a curse, and dangerous.
