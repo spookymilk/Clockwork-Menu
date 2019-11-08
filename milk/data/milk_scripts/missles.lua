9f2d56a2eeb9fe9e7a76ace584a01ccaaf75846a
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-07 22:09:31 -0500

    Not used

--- milk/data/milk_scripts/missles.lua
+++
@@ -1,6 +0,0 @@
-dofile("data/scripts/lib/utilities.lua")
-dofile("data/milk_scripts/functions.lua");
-
-local entity_id=GetUpdatedEntityID();
-
-attackNear(entity_id,"destruction_target",65,10,"data/entities/projectiles/rocket_tank.xml"); -- this is a curse.
