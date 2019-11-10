99aafccab416434327109def1e1bd4b6d5a9ad17
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-10 16:17:15 -0500

    FIxes, Santa

--- milk/data/milk_scripts/midas.lua
+++ milk/data/milk_scripts/midas.lua
@@ -2,4 +2,5 @@ dofile("data/scripts/lib/utilities.lua")
 dofile("data/milk_scripts/functions.lua");
 
 local entity_id = GetUpdatedEntityID();
-convertField(entity_id,"destruction_target",75,"gold");
+convertField(entity_id,"destruction_target",55,"gold");
+convertField(entity_id,"projectile",75,"gold");
