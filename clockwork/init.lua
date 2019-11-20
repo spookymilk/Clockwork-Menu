7de3367ff58745008b9e7fc40d5e804bee7afc31
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-20 18:34:03 -0500

    Fixes

--- clockwork/init.lua
+++ clockwork/init.lua
@@ -4,3 +4,5 @@ end;
 
 print("Clockwork started.");
 ModMagicNumbersFileAdd("data/clockwork_xml/entities/magic_numbers.xml")
+ModLuaFileAppend("data/scripts/gun/gun_actions.lua","data/clockwork_scripts/customactions.lua");
+ModLuaFileAppend("data/scripts/perks/perk_list.lua","data/clockwork_scripts/customperks.lua");
