7f702e09064ec12801521446d1e0114250b534f4
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-20 18:39:56 -0500

    Fixed poop perk

--- clockwork/data/clockwork_scripts/poop_perk.lua
+++ clockwork/data/clockwork_scripts/poop_perk.lua
@@ -1,5 +1,6 @@
 dofile("data/scripts/lib/utilities.lua")
 dofile("data/clockwork_scripts/functions.lua");
 
-local x,y=localplayerPos();
+local entity_id=GetUpdatedEntityID();
+local x,y=EntityGetTransform(entity_id);
 GameCreateParticle("poo",x-2,y+3,5,0,100,false,false);
