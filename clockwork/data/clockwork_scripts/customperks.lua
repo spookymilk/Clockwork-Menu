579281a4e56aa86ab98ba418270fa6f6f0e497ab
spookymilk <57331576+spookymilk@users.noreply.github.com>
2019-11-20 18:50:06 -0500

    Fixed poop perk x2

--- clockwork/data/clockwork_scripts/customperks.lua
+++ clockwork/data/clockwork_scripts/customperks.lua
@@ -1,6 +1,7 @@
 dofile("data/clockwork_scripts/functions.lua");
 
 addPerk(perk_list,"POOP_PERK","Poop","Maybe a glass of prune juice wasn't the best idea.","data/clockwork_gfx/perks/poop.png","data/clockwork_gfx/perks/poop.png",2,nil,
-function()
-EntityLoad("data/clockwork_xml/script_loaders/poop_perk.xml");
+function(entity_perk_item,entity_who_picked,item_name)
+local poo=EntityLoad("data/clockwork_xml/script_loaders/poop_perk.xml");
+EntityAddChild(entity_who_picked,poo);
 end);
--- clockwork/data/clockwork_scripts/poop_perk.lua
+++ clockwork/data/clockwork_scripts/poop_perk.lua
@@ -3,4 +3,4 @@ dofile("data/clockwork_scripts/functions.lua");
 
 local entity_id=GetUpdatedEntityID();
 local x,y=EntityGetTransform(entity_id);
-GameCreateParticle("poo",x-2,y+3,5,0,100,false,false);
+GameCreateParticle("poo",x,y+5,5,0,0,false,false);
